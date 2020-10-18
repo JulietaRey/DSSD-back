e0eade6230f2a7ee78e25617c4660b3cbc375fe9
julieta <julieta@mycnote.com>
2020-10-17 23:19:08 -0300

    basta

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -5,11 +5,12 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_URL || dbConfig.host,
+  host: process.env.DATABASE_HOST || dbConfig.host,
   port: +process.env.DATABASE_PORT || 5432,
   password: process.env.DATABASE_PASSWORD || dbConfig.password,
   username: process.env.DATABASE_USER 
-  || dbConfig.username,
+    || dbConfig.username,
+  database: process.env.DATABASE_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 }
