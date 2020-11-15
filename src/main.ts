43d3075a1751d37610c22951e242c471aa58f3fb
Toto <toto_lp77@hotmail.com>
2020-11-14 21:41:13 -0300

    debug69

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
-    credentials: true
   });
   await app.listen(process.env.PORT || 5000)
   ;
